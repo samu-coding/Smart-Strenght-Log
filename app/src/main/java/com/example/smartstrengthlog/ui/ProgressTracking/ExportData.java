17b330c4e61a60a095ca9d5aa4a7687ab45f4176
Samu <samuelspp97@gmail.com>
2021-04-15 10:53:35 +0200

    Exportación CSV DEMO
    Se ha añadido una exportación demos de dos columnas random. Además se ha empezadola pantalla de exportación.

---
+++ app/src/main/java/com/example/smartstrengthlog/ui/ProgressTracking/ExportData.java
@@ -0,0 +1,16 @@
+package com.example.smartstrengthlog.ui.ProgressTracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartstrengthlog.R;
+
+public class ExportData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export_data);
+    }
+}
